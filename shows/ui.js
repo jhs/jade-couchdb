0a829dd056667642d4bfecf168a083ce44662f6b
Jason Smith <jhs@couchone.com>
2010-11-09 19:42:30 -0800

    Auto-insert a final line in the template to satisfy Jade

--- shows/ui.js
+++ shows/ui.js
@@ -3,8 +3,11 @@ function(doc, req) {
     , jade = require('vendor/jade')
     ;
 
-  var template = (doc && doc.template) || ddoc.template;
   var context = doc || ddoc.default_context;
+  var template = (doc && doc.template) || ddoc.template;
+
+  // For some reason templates want a final line with a token in the first column.
+  template += "\n// end";
 
   var result = jade.render(template, {debug:true, "locals":context, "filename":"jade-template"});
   return { code: 200
