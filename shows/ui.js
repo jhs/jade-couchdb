e34edcbe4fd2f7d44acf5e47507b6926a43d2d68
Jason Smith <jhs@couchone.com>
2010-11-09 19:28:44 -0800

    Remove debugging code

--- shows/ui.js
+++ shows/ui.js
@@ -3,16 +3,10 @@ function(doc, req) {
     , jade = require('vendor/jade')
     ;
 
-  jade.log = log;
   var template = (doc && doc.template) || ddoc.template;
   var context = doc || ddoc.default_context;
 
-  log('Rendering: template size ' + template.length);
-  log(template);
-  log(context);
-  var result = jade.render(template, {debug:true, context:context, "locals":context, "filename":"jade-template"});
-  log('Render complete');
-
+  var result = jade.render(template, {debug:true, "locals":context, "filename":"jade-template"});
   return { code: 200
          , headers: {"Content-Type": "text/html"}
          , body: result
